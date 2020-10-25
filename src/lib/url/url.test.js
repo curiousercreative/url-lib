10d233636f521116deffc2d5df248f157e7bf32b
Winston Hoy <winston@curiousercreative.com>
2020-10-25 11:46:46 -0400

    add "-" to whitelist for variable values, hard to believe this was even working before...

--- src/lib/url/url.test.js
+++ src/lib/url/url.test.js
@@ -1,5 +1,9 @@
-import { generateUrl, reverseUrl } from './';
+import { generateUrl, reverseUrl, test as testUrl } from './';
 
 test('reverseUrl should gracefully handle "/" template', () => {
   expect(reverseUrl('/', '/')).toEqual({});
+});
+
+test('test should handle urls with "-"', () => {
+  expect(testUrl('/blah/:blah', '/blah/blah-blah')).toBe(true);
 })
